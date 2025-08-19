23afc64211d230cb2e8a16ab66bc6f1a1ae2eeca
Pascal PC <pascalgea03@gmail.com>
2025-08-20 10:37:39 +1200

    Added a function for different colorschemes depending on the device

--- lua/pascalgea/lazy/colors.lua
+++ lua/pascalgea/lazy/colors.lua
@@ -6,18 +6,37 @@ function ColorMyPencils(color)
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
-return {
-    {
-        "neanias/everforest-nvim",
-        version = false,
-        lazy = false,
-        priority = 1000, -- make sure to load this before all the other start plugins
-        -- Optional; default configuration will be used if setup isn't called.
-        config = function()
-            require("everforest").setup({
-                -- Your config here
-            })
-            ColorMyPencils();
-        end,
+local uname = vim.loop.os_uname().sysname
+
+if uname == "Darwin" then 
+    -- For the macbook
+    return {
+        {
+            "catppuccin/nvim",
+            name = "catppuccin",
+            priority = 1000,
+            config = function()
+                vim.cmd.colorscheme("catppuccin-macchiato");
+                ColorMyPencils();
+            end,
+        }
     }
-}
+
+elseif uname == "Linux" then
+    -- For the Linux
+    return {
+        {
+            "neanias/everforest-nvim",
+            version = false,
+            lazy = false,
+            priority = 1000, -- make sure to load this before all the other start plugins
+            -- Optional; default configuration will be used if setup isn't called.
+            config = function()
+                require("everforest").setup({
+                    -- Your config here
+                })
+                ColorMyPencils();
+            end,
+        }
+    }
+end
