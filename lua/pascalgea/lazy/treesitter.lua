54c9fa2890cedc797603ee86628c6741bf06c86a
Pascal PC <pascalgea03@gmail.com>
2025-08-20 10:58:39 +1200

    Added some extra variables for treesitter in treesitter.lua. updated lazy-lock.json

--- lua/pascalgea/lazy/treesitter.lua
+++ lua/pascalgea/lazy/treesitter.lua
@@ -5,8 +5,19 @@ return {
         require("nvim-treesitter.configs").setup({
             -- A list of parser names, or "all"
             ensure_installed = {
-                "vimdoc", "javascript", "typescript", "c", "lua", "rust",
-                "jsdoc", "bash", "go",
+                "vimdoc",
+                "javascript",
+                "typescript",
+                "c",
+                "lua",
+                "rust",
+                "jsdoc",
+                "bash",
+                "go",
+                "diff",
+                "html",
+                "python",
+                "regex",
             },
 
             -- Install parsers synchronously (only applied to `ensure_installed`)
