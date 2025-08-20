b820d9536b901f88ae4f701bf893f10a81b126bf
Pascal PC <pascalgea03@gmail.com>
2025-08-20 12:15:21 +1200

    Added some more descriptions to keybinds

--- lua/pascalgea/lazy/telescope.lua
+++ lua/pascalgea/lazy/telescope.lua
@@ -1,6 +1,6 @@
 return {
     "nvim-telescope/telescope.nvim",
-    tag = "0.1.8",
+  python  tag = "0.1.8",
 
     dependencies = {
         "nvim-lua/plenary.nvim"
@@ -14,6 +14,7 @@ return {
         vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Telescope git files' })
         vim.keymap.set('n', '<leader>ps', function() 
             builtin.grep_string({ search = vim.fn.input("Grep > ") });
+            { desc = 'Telescope Grep' }
         end)
     end
 }
