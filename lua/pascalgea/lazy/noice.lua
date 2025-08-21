33d601d6fd8ebefea7725b5ab2e9c0fbc6373e97
Pascal PC <pascalgea03@gmail.com>
2025-08-21 14:23:54 +1200

    Changed it so that the cmd line for noice was at the bottom

--- lua/pascalgea/lazy/noice.lua
+++ lua/pascalgea/lazy/noice.lua
@@ -3,7 +3,9 @@ return {
   "folke/noice.nvim",
   event = "VeryLazy",
   opts = {
-    -- add any options here
+    cmdline = {
+      view = "cmdline",
+    },
   },
   dependencies = {
     -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
