43b5ebe44aa4b9c253ce493f08c0d2a1235da873
Pascal Gea <pge33@uclive.ac.nz>
2025-08-24 21:29:06 +1200

    Added keybinds for switching windows faster

--- lua/pascalgea/remap.lua
+++ lua/pascalgea/remap.lua
@@ -25,5 +25,12 @@ vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+-- vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+vim.keymap.set("n", "<C-J>", "<C-W><C-J>", { silent = true } )
+vim.keymap.set("n", "<C-K>", "<C-W><C-K>", { silent = true } )
+vim.keymap.set("n", "<C-L>", "<C-W><C-L>", { silent = true } )
+vim.keymap.set("n", "<C-H>", "<C-W><C-H>", { silent = true } )
+
+
